d3fc09de7d6a493e7768c5d0b25d682418e3a0db
illuminatich <illuminatichrisky@gmail.com>
2020-11-12 15:43:11 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -46,47 +46,47 @@ def main():
     try:
         if req.get(r'https://graph.microsoft.com/v1.0/me/drive/root',headers=headers).status_code == 200:
             num1+=1
-            print("1调用成功"+str(num1)+'次')
+            print("1调用成功,累计"+str(num1)+'次')
             time.sleep(random.randrange(3, 10))
         if req.get(r'https://graph.microsoft.com/v1.0/me/drive',headers=headers).status_code == 200:
             num1+=1
-            print("2调用成功"+str(num1)+'次')
+            print("2调用成功,累计"+str(num1)+'次')
             time.sleep(random.randrange(3, 10))
         if req.get(r'https://graph.microsoft.com/v1.0/drive/root',headers=headers).status_code == 200:
             num1+=1
-            print('3调用成功'+str(num1)+'次')
+            print('3调用成功,累计'+str(num1)+'次')
             time.sleep(random.randrange(3, 10))
         if req.get(r'https://graph.microsoft.com/v1.0/users ',headers=headers).status_code == 200:
             num1+=1
-            print('4调用成功'+str(num1)+'次')
+            print('4调用成功,累计'+str(num1)+'次')
             time.sleep(random.randrange(3, 10))
         if req.get(r'https://graph.microsoft.com/v1.0/me/messages',headers=headers).status_code == 200:
             num1+=1
-            print('5调用成功'+str(num1)+'次')
+            print('5调用成功,累计'+str(num1)+'次')
             time.sleep(random.randrange(3, 10))
         if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',headers=headers).status_code == 200:
             num1+=1
-            print('6调用成功'+str(num1)+'次')
+            print('6调用成功,累计'+str(num1)+'次')
             time.sleep(random.randrange(3, 10))
         if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/Inbox/messages/delta',headers=headers).status_code == 200:
             num1+=1
-            print('7调用成功'+str(num1)+'次')
+            print('7调用成功,累计'+str(num1)+'次')
             time.sleep(random.randrange(3, 10))
         if req.get(r'https://graph.microsoft.com/v1.0/me/drive/root/children',headers=headers).status_code == 200:
             num1+=1
-            print('8调用成功'+str(num1)+'次')
+            print('8调用成功,累计'+str(num1)+'次')
             time.sleep(random.randrange(3, 10))
         if req.get(r'https://api.powerbi.com/v1.0/myorg/apps',headers=headers).status_code == 200:
             num1+=1
-            print('8调用成功'+str(num1)+'次')
+            print('8调用成功,累计'+str(num1)+'次')
             time.sleep(random.randrange(3, 10))
         if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders',headers=headers).status_code == 200:
             num1+=1
-            print('9调用成功'+str(num1)+'次')
+            print('9调用成功,累计'+str(num1)+'次')
             time.sleep(random.randrange(3, 10))
         if req.get(r'https://graph.microsoft.com/v1.0/me/outlook/masterCategories',headers=headers).status_code == 200:
             num1+=1
-            print('10调用成功'+str(num1)+'次')
+            print('10调用成功,累计'+str(num1)+'次')
             time.sleep(random.randrange(3, 7))
             print('此次运行结束时间为 :', localtime)
     except:
